9abe3d0a295445d48126c48d8993d08c256f7b8c
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-12-10 12:50:34 +0600

    udpate

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -9,7 +9,7 @@ type Role = "patient" | "doctor" | "solo_nurse" | "clinic" | "admin";
 const auth = (...roles: Role[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const token = req.headers.authorization;
+      const token = req.headers.authorization?.split(" ")[1];
       if (!token) {
         throw new AppError("You are not authorize!!", 401);
       }
